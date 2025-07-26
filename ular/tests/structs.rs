4b389970eeeb7dd865c294790903526f8995d4fc
jadenPete <jadenpete@github.com>
2025-07-26 16:18:53 -0400

    Added tests for structs

---
+++ ular/tests/structs.rs
@@ -0,0 +1,233 @@
+mod common;
+
+use crate::common::evaluate_program;
+
+#[test]
+fn struct_definition_works() -> anyhow::Result<()> {
+    evaluate_program(
+        "\
+struct Point {
+    x: i32,
+    y: i32
+}
+",
+        true,
+    )?;
+
+    Ok(())
+}
+
+#[test]
+fn empty_struct_not_allowed() -> anyhow::Result<()> {
+    evaluate_program("struct Foo {}", false)?;
+
+    Ok(())
+}
+
+#[test]
+fn struct_application_typechecked() -> anyhow::Result<()> {
+    let output1 = evaluate_program(
+        "\
+Point {
+    x: 0,
+    y: 1
+};
+",
+        false,
+    )?;
+
+    assert_eq!(
+        output1,
+        "\
+Error: Unknown type `Point`.
+
+ 1 │ Point {
+   │ ^^^^^
+ 2 │     x: 0,
+ 3 │     y: 1
+
+",
+    );
+
+    let output2 = evaluate_program(
+        "\
+struct Point {
+    x: i32,
+    y: i32
+}
+
+Point {
+    x: 0
+};
+",
+        false,
+    )?;
+
+    assert_eq!(
+        output2,
+        "\
+Error: Missing field when constructing a value of type `Point`: `y`
+
+ 4 │ }
+ 5 │ 
+ 6 │ Point {
+   │ ^^^^^^^
+ 7 │     x: 0
+ 8 │ };
+
+",
+    );
+
+    let output3 = evaluate_program(
+        "\
+struct Point {
+    x: i32,
+    y: i32,
+    z: i32
+}
+
+Point {
+    x: 0
+};
+",
+        false,
+    )?;
+
+    assert_eq!(
+        output3,
+        "\
+Error: Missing fields when constructing a value of type `Point`: `y`, `z`
+
+ 5 │ }
+ 6 │ 
+ 7 │ Point {
+   │ ^^^^^^^
+ 8 │     x: 0
+ 9 │ };
+
+",
+    );
+
+    let output4 = evaluate_program(
+        "\
+struct Point {
+    x: i32,
+    y: i32
+}
+
+Point {
+    x: 0,
+    y: 1,
+    z: 0
+};
+",
+        false,
+    )?;
+
+    assert_eq!(
+        output4,
+        "\
+Error: Type `Point` has no field named `z`.
+
+  7 │     x: 0,
+  8 │     y: 1,
+  9 │     z: 0
+    │     ^
+ 10 │ };
+
+",
+    );
+
+    Ok(())
+}
+
+#[test]
+fn struct_application_works() -> anyhow::Result<()> {
+    evaluate_program(
+        "\
+struct Person {
+    age: u8
+}
+
+Person {
+    age: 21
+};
+",
+        true,
+    )?;
+
+    Ok(())
+}
+
+#[test]
+fn select_typechecked() -> anyhow::Result<()> {
+    let output1 = evaluate_program("0.a;", false)?;
+
+    assert_eq!(
+        output1,
+        "\
+Error: Type `i32` has no field named `a`.
+
+ 1 │ 0.a;
+   │ ^^^
+
+",
+    );
+
+    let output2 = evaluate_program(
+        "\
+struct Person {
+    age: u8
+}
+
+Person {
+    age: 21
+}.name;
+",
+        false,
+    )?;
+
+    assert_eq!(
+        output2,
+        "\
+Error: Type `Person` has no field named `name`.
+
+ 3 │ }
+ 4 │ 
+ 5 │ Person {
+   │ ^^^^^^^^
+ 6 │     age: 21
+ 7 │ }.name;
+
+",
+    );
+
+    Ok(())
+}
+
+#[test]
+fn select_works() -> anyhow::Result<()> {
+    let output = evaluate_program(
+        "\
+struct Point {
+    x: i32,
+    y: i32
+}
+
+point = Point {
+    x: 0,
+    y: 1
+};
+
+seq {
+    println_i32(point.x);
+    println_i32(point.y);
+};
+",
+        true,
+    )?;
+
+    assert_eq!(output, "0\n1\n");
+
+    Ok(())
+}
